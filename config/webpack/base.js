a551115fced295314687d5fcb6eb710d91c64655
Adam Hellberg <sharparam@sharparam.com>
2021-10-05 18:39:03 +0200

    Initial commit

---
+++ config/webpack/base.js
@@ -0,0 +1,6 @@
+const { webpackConfig, merge } = require('@rails/webpacker');
+const ForkTSCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = merge(webpackConfig, {
+  plugins: [ new ForkTSCheckerWebpackPlugin() ]
+});
