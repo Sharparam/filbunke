9d5913aa031e84dacc19ba6536be8c44760303b2
Adam Hellberg <sharparam@sharparam.com>
2021-10-10 22:48:03 +0200

    Configure email sending with SendGrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'filbunke@sharparam.com'
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,19 @@
 # Load the Rails application.
 require_relative 'application'
 
+sendgrid_api_key = ENV.fetch('SENDGRID_API_KEY') do
+  Rails.application.credentials.dig :sendgrid, :api_key
+end
+
+ActionMailer::Base.smtp_settings = {
+  user_name: 'apikey',
+  password: sendgrid_api_key,
+  domain: 'sharparam.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
