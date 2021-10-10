d3ef4e34b69d0e32f3eb99ace47b78789769f2fc
Adam Hellberg <sharparam@sharparam.com>
2021-10-10 23:01:15 +0200

    Configure i18n load paths

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module Filbunke
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
   end
 end
