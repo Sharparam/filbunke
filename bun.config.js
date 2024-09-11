7909911a38837707ad6d634293ecfd4791a7f1b8
Adam Hellberg <sharparam@sharparam.com>
2024-09-11 05:05:14 +0200

    Configure TypeScript

--- bun.config.js
+++ bun.config.js
@@ -1,17 +1,17 @@
-import path from 'path';
-import fs from 'fs';
+import path from "path";
+import fs from "fs";
 
 const config = {
   sourcemap: "external",
-  entrypoints: ["app/javascript/application.js"],
-  outdir: path.join(process.cwd(), "app/assets/builds"),
+  entrypoints: ["app/javascript/application.ts", "app/javascript/theme.ts"],
+  outdir: path.join(process.cwd(), "app/assets/builds")
 };
 
 const build = async (config) => {
   const result = await Bun.build(config);
 
   if (!result.success) {
-    if (process.argv.includes('--watch')) {
+    if (process.argv.includes("--watch")) {
       console.error("Build failed");
       for (const message of result.logs) {
         console.error(message);
@@ -26,11 +26,15 @@ const build = async (config) => {
 (async () => {
   await build(config);
 
-  if (process.argv.includes('--watch')) {
-    fs.watch(path.join(process.cwd(), "app/javascript"), { recursive: true }, (eventType, filename) => {
-      console.log(`File changed: ${filename}. Rebuilding...`);
-      build(config);
-    });
+  if (process.argv.includes("--watch")) {
+    fs.watch(
+      path.join(process.cwd(), "app/javascript"),
+      { recursive: true },
+      (eventType, filename) => {
+        console.log(`File changed: ${filename}. Rebuilding...`);
+        build(config);
+      }
+    );
   } else {
     process.exit(0);
   }
